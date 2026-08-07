22084b132e128ca76c20419ef07480a5f7b1a0f4
Akane-Yamisaki <cernyha986@gmail.com>
2026-08-07 17:01:40 +0500

    Апдейт конфига для сохранялок

--- config.py
+++ config.py
@@ -3,7 +3,6 @@
 # Данные игрока
 player_name = ""
 money = 0
-Dlc_list = {"Dlc_18": False}
 
 # Отношения с Аканэ
 trust_level = 0        # Растет от подарков, еды и поглаживаний
@@ -11,8 +10,9 @@ current_day = 1        # Текущий день (влияет на фан-се�
 
 # Состояние Аканэ
 is_hungry = True       # Нужно кормить каждый день
-
 is_onScreen = True
+akane_x, akane_y = 0, 0
+akane_scene = ""
 
 # --- МОБИЛЬНОЕ УПРАВЛЕНИЕ ---
 is_mobile = False
@@ -23,8 +23,8 @@ joystick_vector = [0.0, 0.0]  # Направление движения [X, Y] �
 unlocked_costumes = {
     "default": True,
     "maid": False,
-    "swimsuit": False,
-    "without": Dlc_list["Dlc_18"]
+    "swimsuit": False
 }
+
 inventory = {}
 current_costume = "default"
