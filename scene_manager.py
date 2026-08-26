591eca8e9a789bc71ab4c79bf440e7028413049b
Akane-Yamisaki <cernyha986@gmail.com>
2026-08-27 01:09:20 +0500

    Update scene_manager.py

--- scene_manager.py
+++ scene_manager.py
@@ -3,7 +3,6 @@ from scenes.Akane_Bedroom import BedRoom
 from scenes.menu import Menu
 from scenes.Saves import Saves
 
-
 class SceneManager:
     def __init__(self, screen, SCREEN_W, SCREEN_H):
         self.screen = screen
@@ -14,8 +13,6 @@ class SceneManager:
 
         self.scenes = {
             'menu': Menu,
-            'saves': Saves,
-            # Backward-compatible alias used by older scene transitions.
             'Saves': Saves,
             'Bedroom_Day1': HouseScene,
             'Bedroom_pixel': BedRoom
